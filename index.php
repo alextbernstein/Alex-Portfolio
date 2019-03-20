eb91d355a76225f030ac0687a77285edfb533ec3
Alex Bernstein <alextbernstein@gmail.com>
2019-03-19 18:06:22 -0700

    added title

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="script.js"></script>
+    <title>Alex Bernstein | Web Developer</title>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </head>
